b6055ca7a33a30529501400fe6cd508775d9bbd0
sarboledab <sarboledabotero@gmail.com>
2021-05-24 10:46:33 -0500

    Se sube api

--- app/Http/Controllers/user/FoundationsController.php
+++ app/Http/Controllers/user/FoundationsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\user;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Foundation;
@@ -8,25 +9,20 @@ use App\Models\Donation;
 
 class FoundationsController extends Controller
 {
-    
+
     public function list()
     {
-        
-        
-        try{
+        try {
             $data["foundations"] = Foundation::all();
-            return view('user.foundations.list')->with("data",$data);
-            
-        } catch (\Throwable $th){
+            return view('user.foundations.list')->with("data", $data);
+        } catch (\Throwable $th) {
             return view('user.foundations.list')->with('danger', "Couldn't get the list!");
         }
-        
     }
 
     public function show($id)
     {
         $data["foundation"] = Foundation::findOrFail($id);
-        return view('user.foundations.show')->with("data",$data);
+        return view('user.foundations.show')->with("data", $data);
     }
-
 }
