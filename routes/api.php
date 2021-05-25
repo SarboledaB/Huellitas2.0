d30af255c2e04e06dfb99e3cb3c0da3edd2aeedf
sarboledab <sarboledabotero@gmail.com>
2021-05-24 22:44:28 -0500

    se cambia de nombre

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/huellistas/foundations', 'App\Http\Controllers\Api\FoundationsController@list');
+Route::get('/huellitas/foundations', 'App\Http\Controllers\Api\FoundationsController@list');
