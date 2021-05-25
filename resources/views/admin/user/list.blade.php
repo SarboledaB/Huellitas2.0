fda658338547ea3f3f2f43d46e1284744eff4306
Anthony <agarciam@eafit.edu.co>
2021-05-24 23:27:49 -0500

    listUsers

--- resources/views/admin/user/list.blade.php
+++ resources/views/admin/user/list.blade.php
@@ -1,27 +1,35 @@
 @extends('layouts.app')
 
-@section("title", $data["title"])
+
 
 @section('content')
-<section class="page-section">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ $data["user"]->getUsername() }}</div>
-                    <div class="card-body">
-                        <b>Id:</b> {{ $data["user"]->getId() }}<br />
-                        <b>Username:</b> {{ $data["user"]->getUsername() }}<br />
-                        <b>First Name:</b> {{ $data["user"]->getFirstName() }}<br />
-                        <b>Last Name:</b> {{ $data["user"]->getLastName() }}<br />
-                        <b>Email:</b> {{ $data["user"]->getEmail() }}<br />
-                        <b>Password:</b> {{ $data["user"]->getPassword() }}<br />
-                        <br>
-                        <a class="btn btn-outline-danger" href="{{ route('admin.user.delete', ['id' => $data['user']['id']] ) }}">@lang('general.delete')</a>
-                    </div>
-                </div>
-            </div>
+<section class="page-section portfolio">
+    <div class="row p-5">
+        <div class="col-md-12">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">@lang('general.status')</th>
+                        <th scope="col">@lang('general.total_cost')</th>
+                        <th scope="col">@lang('general.payment_method')</th>
+                        <th scope="col">@lang('general.total_cost')</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($data["users"] as $user)
+                    <tr>
+                        <td scope="row"><a href="{{ route('admin.user.show', ['id' => $user->getId()]) }}">{{ $user->getId() }}</a></td>
+                        <td>{{ $user->getUsername() }}</td>
+                        <td>{{ $user->getFirstName() }}</td>
+                        <td>{{ $user->getLastName() }}</td>
+                        <td>{{ $user->getEmail() }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
+
 </section>
 @endsection
